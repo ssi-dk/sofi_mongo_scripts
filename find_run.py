e749e18a56f783aade7c23755808046e9f5bf45c
Finn Gruwier Larsen <figl@ssi.dk>
2022-08-25 14:36:40 +0200

    Rename variable.

--- find_run.py
+++ find_run.py
@@ -7,7 +7,7 @@ from init_mongo_connection import db
 parser = argparse.ArgumentParser(
     description='Find and optionally delete a run and related documents from MongoDB.')
 parser.add_argument('inst', type=str, help="Institution, either 'ssi' or 'fvst'")
-parser.add_argument('name', type=str, help="Partial run name search for")
+parser.add_argument('part', type=str, help="Partial run name search for")
 parser.add_argument('-d', type=str, help="Interactively delete documents")
 args = parser.parse_args()
 
@@ -16,6 +16,6 @@ if args.inst not in ['ssi', 'fvst']:
     exit(1)
 
 prefix: str = '.*N_WGS_' if args.inst == 'ssi' else '[Rr][Uu][Nn]'
-regex = re.compile(prefix + args.name + '.*')
+regex = re.compile(prefix + args.part + '.*')
 runs = db.runs.find({'name':  regex})
 print(list(runs))
