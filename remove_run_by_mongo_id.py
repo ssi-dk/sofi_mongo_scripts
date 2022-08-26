e6b2850aa91d89f87f53c52fbd41a2fd25eb8765
Finn Gruwier Larsen <figl@ssi.dk>
2022-08-26 08:46:52 +0200

    Refactor remove_run_by_mongo_id.py

--- remove_run_by_mongo_id.py
+++ remove_run_by_mongo_id.py
@@ -5,6 +5,40 @@ import argparse
 import api
 from api import sample_components
 
+def delete_run_documents(run):
+    # Delete sample and sample_component documents
+    for run_sample in run['samples']:
+        sample = api.samples.get_sample_by_id(run_sample['_id'])
+        if sample is None:
+            print(f"Consistency warning: a sample that is referenced in the run does not exist in samples collection:")
+            print(run_sample)
+        else:
+            if run_sample['name'] != sample['name']:
+                print(f"Consistency warning: name consistency check failed for sample id {run_sample['_id']}.")
+                print(f"Run sample name is {run_sample['name']}")
+                print(f"Sample name is {sample['name']}")
+        
+            component_names = [component['name'] for component in sample['components']]
+            print ("Sample [Components]:")
+            print(sample['name'], component_names)
+            sample_components = list(api.sample_components.find_sample_component_ids_by_sample_id(sample['_id']))
+            sample_component_object_ids = [sc['_id'] for sc in sample_components]
+            for oid in sample_component_object_ids:
+                # Delete sample_component document
+                if not args.fake:
+                    api.sample_components.delete_sample_component_by_id(oid)
+            print(f"Deleted {len(sample_components)} sample_component documents (unless fake)")
+            # Delete sample document
+            if not args.fake:
+                api.samples.delete_sample_by_id(run_sample['_id'])
+            print(f"Deleted sample document with id {run_sample['_id']} (unless fake)")
+
+    # Delete run document
+    if not args.fake:
+        api.runs.delete_run_by_id(run['_id'])
+    print(f"Deleted run document with id {run['_id']} (unless fake)")
+
+
 # Call this once before making any other calls.
 api.add_URI(getenv('MONGO_CONNECTION'))
 
@@ -23,34 +57,4 @@ answer = input("Continue (y/n)? ")
 if not answer in ['Y', 'y']:
     exit()
 
-# Delete sample and sample_component documents
-for run_sample in run['samples']:
-    sample = api.samples.get_sample_by_id(run_sample['_id'])
-    if sample is None:
-        print(f"Consistency warning: a sample that is referenced in the run does not exist in samples collection:")
-        print(run_sample)
-    else:
-        if run_sample['name'] != sample['name']:
-            print(f"Consistency warning: name consistency check failed for sample id {run_sample['_id']}.")
-            print(f"Run sample name is {run_sample['name']}")
-            print(f"Sample name is {sample['name']}")
-    
-        component_names = [component['name'] for component in sample['components']]
-        print ("Sample [Components]:")
-        print(sample['name'], component_names)
-        sample_components = list(api.sample_components.find_sample_component_ids_by_sample_id(sample['_id']))
-        sample_component_object_ids = [sc['_id'] for sc in sample_components]
-        for oid in sample_component_object_ids:
-            # Delete sample_component document
-            if not args.fake:
-                api.sample_components.delete_sample_component_by_id(oid)
-        print(f"Deleted {len(sample_components)} sample_component documents (unless fake)")
-        # Delete sample document
-        if not args.fake:
-            api.samples.delete_sample_by_id(run_sample['_id'])
-        print(f"Deleted sample document with id {run_sample['_id']} (unless fake)")
-
-# Delete run document
-if not args.fake:
-    api.runs.delete_run_by_id(run['_id'])
-print(f"Deleted run document with id {run['_id']} (unless fake)")
+delete_run_documents(run)
