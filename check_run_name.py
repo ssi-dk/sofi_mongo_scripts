8e75e9ea3339d64c074a4168b33a87405b040e32
Finn Gruwier Larsen <figl@ssi.dk>
2022-11-01 13:08:33 +0100

    Remove old scripts that do not work any more.

--- check_run_name.py
+++
@@ -1,8 +0,0 @@
-from os import getenv
-import api
-
-# Call this once before making any other calls.
-api.add_URI(getenv('MONGO_CONNECTION'))
-
-run_name = input("Run name: ")
-print(api.runs.check_run_name(run_name))
--- get_run_list.py
+++
@@ -1,9 +0,0 @@
-from os import getenv
-import api
-
-# Call this once before making any other calls.
-api.add_URI(getenv('MONGO_CONNECTION'))
-
-runs = api.runs.get_run_list()
-for run in runs:
-    print(run['_id'], run['name'])
